960ca9461d8e2a7355d941c2639ecbbfcba5ef53
noveli <jromango23@gmail.com>
2026-06-18 23:50:39 -0400

    Agregar redirección inicial a login

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route 
